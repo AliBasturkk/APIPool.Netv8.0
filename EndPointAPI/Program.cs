4a4071325ea2879f553d0dfa707f29e008d02e42
Ali Baştürk <alibasturkk@gmail.com>
2025-08-01 21:29:34 +0300

    Add project files.

---
+++ EndPointAPI/Program.cs
@@ -0,0 +1,23 @@
+using EndPointAPI.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IToolRepository, ToolRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/tckn_check/{tckn}", async (IToolRepository tool, string tckn) => await tool.CheckTCKNAsync(tckn));
+
+app.Run();
